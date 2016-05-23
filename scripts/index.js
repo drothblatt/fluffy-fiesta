bb66076892ed983268f54563cc52e82cf079a329
Ishraq Bhuiyan <ishraqbhuiyan@gmail.com>
2016-05-23 01:48:24 -0400

    Make some changes

--- scripts/index.js
+++ scripts/index.js
@@ -14,15 +14,6 @@ interact('.draggable')
 
     // call this function on every dragmove event
     onmove: dragMoveListener,
-    // call this function on every dragend event
-    onend: function (event) {
-      var textEl = event.target.querySelector('p');
-
-      textEl && (textEl.textContent =
-        'moved a distance of '
-        + (Math.sqrt(event.dx * event.dx +
-                     event.dy * event.dy)|0) + 'px');
-    }
   });
 
   function dragMoveListener (event) {
